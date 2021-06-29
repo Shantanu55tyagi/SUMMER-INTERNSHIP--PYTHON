daba2abe9747ff7101ba5aa060e69aa5287beaa1
Shantanu55tyagi <65650362+Shantanu55tyagi@users.noreply.github.com>
2021-06-29 16:24:08 +0530

    Add files via upload

---
+++ shanu4.py
@@ -0,0 +1,8 @@
+#variable in python
+name="shanu"
+print(name) #don't use inverted commas in variable
+name="tyagi"
+print(name)#variable values varies with time
+name=55
+print(name)
+print(name+6)#int + int
