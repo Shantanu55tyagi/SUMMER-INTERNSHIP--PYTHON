568087964a36fbe4c8d20e53711dc2a80b985664
Shantanu55tyagi <65650362+Shantanu55tyagi@users.noreply.github.com>
2021-06-29 16:23:51 +0530

    Add files via upload

---
+++ shanu3.py
@@ -0,0 +1,12 @@
+#python as a calculator
+print("65+25")# bcoz of inverted commas values are treated as a string
+print(65+25)#addition
+print(65-20)#subtraction
+print(20-65)#subtraction
+print(20*65)#multiplication
+print(20/65)#floating
+print(20//65)#integer
+print(2**3)#power
+print(2+3*4)#precedence rule
+print((2+3)*5/2%6)#associativity rule
+print(2**3**2)#associativity rule-right to left
