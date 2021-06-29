4f6d15c12e76e3a632a36299069ea8c988298928
Shantanu55tyagi <65650362+Shantanu55tyagi@users.noreply.github.com>
2021-06-29 16:22:52 +0530

    Add files via upload

---
+++ shanu1.py
@@ -0,0 +1,13 @@
+print('helloc')
+print("helooooo")
+print("hello'shanu'")
+print('helloo"shanu"')
+
+#escape sequences
+print("I\'m SHANU ") #for single inverted commas
+print("I am \"SHANU\"")#for doube inverted commas
+print("I am \ shanu") #backslash
+print("i am shanu\\") #if we are putting in last so we need to put two backslash
+print("i am shanu \n tyagi") #new line
+print(" i am \t shanu tyagi") #tab space
+print("i am sh \banu") #correct way to put backspace just before the second word
